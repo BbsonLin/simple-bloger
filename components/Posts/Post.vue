82f0a4f5dfd70af161779890170deabfa93bcdec
Bobson Lin <bobson801104@gmail.com>
2018-03-16 11:57:53 +0800

    Add Single post page

---
+++ components/Posts/Post.vue
@@ -0,0 +1,29 @@
+<template>
+  <article class="text-center">
+    <h1>{{ title }} (ID: {{ id }})</h1>
+    <p>{{ previewText }}</p>
+  </article>
+</template>
+
+<script>
+export default {
+  props: {
+    id: {
+      type: Number,
+      required: true
+    },
+    thumbnail: {
+      type: String,
+      required: true
+    },
+    title: {
+      type: String,
+      required: true
+    },
+    previewText: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
---
+++ pages/post/_id/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="cantainer">
+    <Post/>
+  </div>
+</template>
+
+<script>
+import Post from '@/components/Posts/Post'
+
+export default {
+  components: {
+    Post
+  }
+}
+</script>
+
+
+
